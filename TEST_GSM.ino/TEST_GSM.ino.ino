aa73b1e58a62a4b86150ebf5557d8a0c4c098190
Владимир Драчёв <vovantrojan131313@gmail.com>
2021-02-05 18:36:55 +0500

    Разработка

--- TEST_GSM.ino/TEST_GSM.ino.ino
+++ TEST_GSM.ino/TEST_GSM.ino.ino
@@ -1,40 +1,67 @@
 #include <SoftwareSerial.h>
- 
-SoftwareSerial mySerial(11, 12); // RX, TX
 
-String bufferSIM;
+class SmsWork
+{
+public:
+  SoftwareSerial* mySerial;
+  String bufferSIM;
 
-void setup()  
+  SmsWork(){
+    mySerial = new SoftwareSerial(11, 12);
+    mySerial->begin(4800);
+    mySerial->write("AT+CMGF=1\r");
+  }
+
+  void Work()
+  {
+    if (mySerial->available())
+    {
+      char readSIM = mySerial->read();
+      bufferSIM = bufferSIM + readSIM;
+      bool ismess = false;
+
+      if (readSIM == '\r')
+      {
+        if (bufferSIM.substring(1, 6) == "+CMTI")
+        {
+          String num = bufferSIM.substring(13);
+          num = "AT+CMGR="+num+"\r";
+          mySerial->write(num.c_str());
+        }
+
+        if(ismess)
+        {
+            ismess = false;
+            Serial.write(bufferSIM.c_str());
+        }
+
+        if (bufferSIM.substring(1, 6) == "+CMGR")
+        {
+            ismess = true;
+        }
+
+
+        for (int i = 0; i < bufferSIM.length(); i++)
+          Serial.write(bufferSIM[i]);
+
+        bufferSIM = "";
+      }
+    }
+    if (Serial.available())
+    {
+      mySerial->write(Serial.read());
+    }
+  }
+};
+
+SmsWork sms_device;
+
+void setup()
 {
   Serial.begin(9600);
-  mySerial.begin(4800);
 }
-
-
 
 void loop() // выполняется циклически
 {
-  if (mySerial.available())
-  {
-    char readSIM = mySerial.read();
-    bufferSIM = bufferSIM + readSIM;
-    
-    if(readSIM == '\r')
-    {
-      if(bufferSIM.substring(1, 6) == "+CMTI")
-      {
-        mySerial.write("AT+CMGR=15\r");
-      }
-
-      for(int i = 0; i < bufferSIM.length(); i++)
-        Serial.write(bufferSIM[i]);
-      
-      bufferSIM = "";
-    }
-    
-  }
-  if (Serial.available())
-  {
-        mySerial.write(Serial.read());
-  }
+  sms_device.Work();
 }
