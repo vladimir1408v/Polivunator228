b37e6bdca0c81f6e45294bcc1d3fb1800c85567f
Владимир Драчёв <vovantrojan131313@gmail.com>
2021-02-10 18:31:48 +0500

    Разработка

--- AppAndroid/app/src/main/java/com/mk/master/polivun/MainActivity.java
+++ AppAndroid/app/src/main/java/com/mk/master/polivun/MainActivity.java
@@ -3,12 +3,31 @@ package com.mk.master.polivun;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    String m_sTell = "+79630332479";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void PolivOn(View view) throws Exception {
+        sendSmsMessage("ON");
+    }
+
+    public void PolivOff(View view) throws Exception {
+        sendSmsMessage("OFF");
+    }
+
+
+    private void sendSmsMessage(String message) throws Exception
+    {
+        SmsManager smsMgr = SmsManager.getDefault();
+        smsMgr.sendTextMessage(m_sTell, null, message, null, null);
     }
 }
